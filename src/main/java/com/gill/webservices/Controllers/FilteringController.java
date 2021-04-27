e7970fed76266a6c656eed105e7c0884e69932fc
Ahmad Gill <gillahmad94.gmail.com>
2021-04-27 22:10:54 +0500

    Dynamic Filtering added

--- src/main/java/com/gill/webservices/Controllers/FilteringController.java
+++ src/main/java/com/gill/webservices/Controllers/FilteringController.java
@@ -3,9 +3,13 @@ package com.gill.webservices.Controllers;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import com.gill.webservices.model.SomeBean;
 
 @RestController
@@ -16,9 +20,41 @@ public class FilteringController {
 		return new SomeBean("value1", "value2", "value3");
 	}
 	
-	@GetMapping("/filtering-list")
-	public List<SomeBean> retrieveListOfSomeBean() {
-		return Arrays.asList( new SomeBean("value1", "value2", "value3"),
-				new SomeBean("value1", "value2", "value3"));
+	@GetMapping("/dynamic-filtering")
+	public MappingJacksonValue retrieveSomeBeanAfterDynamicFiltering() {
+		SomeBean someBean = new SomeBean("value1", "value2", "value3");
+		
+		//1 Create property filter
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field2");
+		
+		//2 Create Filter Provider
+		FilterProvider filters = new SimpleFilterProvider()
+				.addFilter("SomeBeanFilter", filter);
+		//3 Create Mapping JacksoneValue
+		MappingJacksonValue mapping = new MappingJacksonValue(someBean);
+		//4 Set FilterValue
+		mapping.setFilters(filters);
+		return mapping;
 	}
+	@GetMapping("/filtering-list")
+	public MappingJacksonValue retrieveListOfSomeBean() {
+		List<SomeBean> list =Arrays.asList( new SomeBean("value1", "value2", "value3"),
+				new SomeBean("value1", "value2", "value3"));
+				
+		//1 Create property filter
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1", "field2");
+		
+		//2 Create Filter Provider
+		FilterProvider filters = new SimpleFilterProvider()
+				.addFilter("SomeBeanFilter", filter);
+		//3 Create Mapping JacksoneValue
+		MappingJacksonValue mapping = new MappingJacksonValue(list);
+		//4 Set FilterValue
+		mapping.setFilters(filters);
+		return mapping;
+	}
+//	public List<SomeBean> retrieveListOfSomeBean() {
+//		return Arrays.asList( new SomeBean("value1", "value2", "value3"),
+//				new SomeBean("value1", "value2", "value3"));
+//	}
 }
--- src/main/java/com/gill/webservices/model/SomeBean.java
+++ src/main/java/com/gill/webservices/model/SomeBean.java
@@ -1,9 +1,11 @@
 package com.gill.webservices.model;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties(value = {"field1", "field2"})
+//@JsonIgnoreProperties(value = {"field1", "field2"})
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
 	private String field1;
 	private String field2;
